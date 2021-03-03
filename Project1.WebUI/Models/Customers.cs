6baa63a20b57568ba8cfd008f1cfaac6ed62cea8
Ashley Brown <1thing.ashleyb@gmail.com>
2021-03-02 22:07:13 -0600

    added some html validation

---
+++ Project1.WebUI/Models/Customers.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Project1.WebUI.Models
+{
+    public class Customers
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Zip { get; set; }
+    }
+}
